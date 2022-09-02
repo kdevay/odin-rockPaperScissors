24c340c55f08b18accd1ee23d775edbf3d77a19a
Katherine de Vay <katdevay2@gmail.com>
2022-09-02 15:32:10 -0500

    Create basic HTML and JS files

---
+++ rps.js
@@ -0,0 +1,29 @@
+// A function that randomly returns either ‘Rock’ ‘Paper’ or ‘Scissors’.  
+function getComputerChoice {
+    return;
+}
+
+// A function that plays a single round of Rock Paper Scissors.
+ ("You Lose! Paper beats Rock"); 
+function playRound(playerSelection, computerSelection) {
+    // make playerSelection case-insensitive
+    // conditionals for rock/paper/scissor comparison
+    // return a string declaring the round winner ("You <Lose/Win>! <Paper> beats <Rock>")
+    return;
+}
+
+// A function that plays a 5-round game of RPS. 
+function game(playerSelection) {
+    // variable for keeping score
+    let userScore = 0;
+    let computerScore = 0;
+    // call “playRound” function inside this function  
+    // increment score based on outcome of playRound
+    // report a winner or loser at the end
+    return;
+}
+
+// display the winner results of each round and the total game in the console and on the screen
+// Use prompt() to get user input.
+// toggle display table upon clicking submit button
+
